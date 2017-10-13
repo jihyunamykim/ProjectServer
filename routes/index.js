e20b74e54c787560520c4a628238181f93e895a9
jihyunamykim <dkrnahs916@gmail.com>
2017-10-13 17:05:15 +0900

    오픈 API 명세서

--- routes/index.js
+++ routes/index.js
@@ -6,37 +6,79 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Project' });
 });
 
+/**********/
+/* LOGIN  */
+/**********/
+
+//1.ID Login
 router.post('/user/login', function(req,res){
-	var id = req.body.id;
+	var customername = req.body.customername;
 	var password = req.body.password;
-	res.send(JSON.stringify({id:id,password:password}));
+	res.send(JSON.stringify({customername:customername,password:password}));
 });
 
+//2.ID ADD
 router.post('/user', function(req,res){
-	var id = req.body.id;
+	var customername = req.body.customername;
 	var password = req.body.password;
-	res.send(JSON.stringify({id:id,password:password}));
+	res.send(JSON.stringify({customername:customername,password:password}));
 });
 
+//3.ID Search
 router.get('/user',function(req,res){
-	var rowid = req.query.rowid;
-    res.send(JSON.stringify({rowid:rowid}));
+	var customerid = req.query.customerid;
+    res.send(JSON.stringify({customerid:customerid}));
 });
 
+//4.ID Edit
 router.put('/user',function(req,res){
-	var rowid = req.query.rowid;	
-	var id = req.body.id;
+	var customerid = req.query.customerid;	
+	var customername = req.body.customername;
 	var password = req.body.password;
-	res.send(JSON.stringify({rowid:rowid,id:id,password:password}));
+	res.send(JSON.stringify({customerid:customerid,customername:customername,password:password}));
 });
 
+//5.ID Delete
 router.delete('/user',function(req,res){
-	var rowid = req.query.rowid;
-    res.send(JSON.stringify({rowid:rowid}));
+	var customerid = req.query.customerid;
+    res.send(JSON.stringify({customerid:customerid}));
 });
 
+//6.All ID Search
 router.get('/user/list',function(req,res){
     res.send(JSON.stringify([]));
 });
+
+
+/*********/
+/* Group */
+/*********/
+
+//1.Group ADD
+router.post('/group', function(req,res){
+	var groupname = req.body.groupname;
+	var groupid = req.body.groupid;
+	res.send(JSON.stringify({groupname:groupname,groupid:groupid}));
+});
+
+//2.Group Edit
+router.put('/group',function(req,res){
+	var groupid = req.query.groupid;	
+	var groupname = req.body.groupname;
+	var password = req.body.password;
+	res.send(JSON.stringify({customerid:customerid,customername:customername,password:password}));
+});
+
+//5.ID Delete
+router.delete('/group',function(req,res){
+	var customerid = req.query.customerid;
+    res.send(JSON.stringify({customerid:customerid}));
+});
+
+
+
+
+
+
 
 module.exports = router;
